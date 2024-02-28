f37086abda52185054b03e81060b737693d3e166
Abdias637 <137947342+Abdias637@users.noreply.github.com>
2024-02-28 13:07:45 -0600

    a

--- src/components/BorrarProducto.js
+++ src/components/BorrarProducto.js
@@ -1,21 +1,28 @@
 
-import { useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { URL_API } from "../config/rutas";
 
-export function BorrarProducto(){
+export function BorrarProducto() {
     const params = useParams();
-    const navigate =useNavigate();
-    useEffect(()=>{
-        async function borrar(){
-            const res= await axios.get(`http://localhost:3000/api/borrarProducto/${params.id}`);
-            console.log(res);
-            navigate("/");
-            window.location.href = "http://localhost:3001/productos";
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        async function borrar() {
+            try {
+                const res = await axios.delete(`${URL_API}/borrarProducto/${params.id}`);
+                console.log(res);
+                navigate("/");
+            } catch (error) {
+                console.error('Error al borrar el producto:', error);
+            }
         }
         borrar();
-    },[navigate, params.id]);
-    return(
-        <h1>producto borrado</h1>
+    }, [navigate, params.id]);
+
+    return (
+        <h1>Borrar usuario</h1>
     );
 }
+
